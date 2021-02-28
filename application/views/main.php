ffff3c166df66a0158f5c9077f49c68136e5cc36
ryurewsss <68342195+ryurewsss@users.noreply.github.com>
2021-02-28 16:55:47 +0700

    เพิ่ม logo เปลี่ยน controller

--- application/views/main.php
+++ application/views/main.php
@@ -9,7 +9,8 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title><?= $title_name ?></title>
+  <!-- <title><?= $title_name ?></title> -->
+  <title>Team 4 Mini Project</title>
 
   <!-- Custom fonts for this template-->
   <link href="<?= base_url() ?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
--- application/views/record/login.php
+++ application/views/record/login.php
@@ -312,7 +312,7 @@
 
         <!-- Icon -->
         <div class="fadeIn first">
-            <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+            <img src="assets/img/stocks.png" id="icon" alt="User Icon" />
         </div>
 
         <!-- Login Form -->
