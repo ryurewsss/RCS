1836dc1ef5598b27f28031e092fb5ff0f5eb121a
jenphat28 <68342383+jenphat28@users.noreply.github.com>
2021-03-02 15:06:15 +0700

    ค้นหา เจนนนนภัทรรรรจ้า
    ทำหน้าค้นหา จบๆ

---
+++ application/views/record/searchTable.php
@@ -0,0 +1,105 @@
+<!-- Open Table -->
+
+<head>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+</head>
+<div class="card">
+    <div class="card-header bg-dark">
+        <a class="m-b-0 text-white" style="font-size: 17px;">ค้นหารายรับ - รายจ่าย</a>
+    </div>
+    <div class="card-body">
+        <form class="form-material">
+            <label style="text-indent: 20px; word-spacing: 10px;">วันที่ <a style="color: red;  margin-right: 37px;"> *</a> </label>: &ensp;
+            <input style="width: 300px; " type='text' class="form-control buttonClass" placeholder="กรุณาเลือกวัน" /><br><br>
+            <div id="searchTable"></div>
+            <div class="form-group">
+                <div>
+                    <a style="font-size:14pt; margin-right: 30px">ยอดเงินรวม &ensp; 
+                    <input style="width: 110px; height:35px; font-size:12pt; text-align:right;" type="text" class="form-control" id ="sum" placeholder="0.00" disabled>
+                    &ensp; บาท
+                    </a>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<script>
+    getList();
+
+    $('.buttonClass').daterangepicker({
+        autoclose: true,
+        locale: {
+            format: 'DD/MM/YYYY'
+        },
+        endDate: new Date(),
+        drops: "down",
+        buttonClasses: "btn",
+        applyClass: "btn-info",
+        cancelClass: "btn-danger",
+
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        "alwaysShowCalendars": true,
+    }, function(start, end, label) {
+        console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+
+    });
+
+    $('.buttonClass').val('');
+
+    $('.buttonClass').change(function() {
+        date = this.value.trim().split(' - ');
+        firstRange = date[0].split('/');
+        date[0] = firstRange[2] + "-" + firstRange[1] + "-" + firstRange[0];
+
+        secondRange = date[1].split('/');
+        date[1] = secondRange[2] + "-" + secondRange[1] + "-" + secondRange[0];
+        searchDate = "transaction_date >= '" + date[0] + "' and  transaction_date <= '" + date[1] + " 23:59:59.999'";
+        console.log(searchDate)
+        getList(searchDate);
+    });
+
+    function getList(where = '') {
+        var data = {};
+        data['tableName'] = 'ie_transaction';
+        data['colName'] = '';
+        data['where'] = where;
+        data['order'] = '';
+        data['arrayJoinTable'] = '';
+        data['groupBy'] = '';
+        data['pathView'] = 'record/tableSearch';
+
+        $.ajax({
+            method: "POST",
+            url: "getTable",
+            data: data,
+        }).done(function(returnedData) {
+            $('#searchTable').html(returnedData.html);
+            $('.mytb').dataTable({
+                "oLanguage": {
+                    "sLengthMenu": "แสดง _MENU_ ข้อมูล",
+                    "sSearch": "ค้นหา : ",
+                    "sInfo": "แสดง _START_ ถึง _END_ จาก _TOTAL_ ข้อมูล",
+                    "oPaginate": {
+                        "sPrevious": "ย้อนกลับ",
+                        "sNext": "ถัดไป"
+                    },
+                    "sZeroRecords": "ไม่พบข้อมูลที่ต้องการ",
+                    "sInfoFiltered": "(ค้นหาจากทั้งหมด _MAX_ ข้อมูล)",
+                    "sInfoEmpty": "แสดง 0 ถึง 0 จาก 0 ข้อมูล"
+                }
+            });
+            console.log(returnedData.table)
+        });
+    }
+</script>
---
+++ application/views/record/tableSearch.php
@@ -0,0 +1,45 @@
+<div class="row">
+    <table id="myTable" class="table table-bordered table-striped mytb">
+        <thead>
+            <tr class="text-center">
+                <th>วันที่</th>
+                <th class="text-left">รายละเอียด</th>
+                <th>รายรับ</th>
+                <th>รายจ่าย</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Start loop show db to table -->
+            <?php
+            $sum = 0;
+            if (isset($table) && $table) {
+                $i = 1 //กำหนดลำดับ 
+            ?>
+                <?php foreach ($table as $key => $val) {
+                    $sum = $sum + $val->transaction_cash;
+                ?>
+                    <?php if ($val->transaction_delete_status == "active") { ?>
+                        <tr class="text-center">
+                            <td><?= substr($val->transaction_date, 0, 10) ?></td>
+                            <td><?= $val->transaction_description ?></td>
+                            <?php if ($val->transaction_cash < 0) { ?>
+                                <td>-</td>
+                                <td class="text-danger font-weight-bold"><?= $val->transaction_cash * -1 ?></td>
+                            <?php } else { ?>
+                                <td class="text-success font-weight-bold"><?= $val->transaction_cash ?></td>
+                                <td>-</td>
+                            <?php } ?>
+                        </tr>
+                    <?php } ?>
+                <?php } ?>
+            <?php } ?>
+            <!-- End loop -->
+        </tbody>
+    </table>
+</div>
+
+<script>
+    var sum = <?php echo json_encode($sum) ?>;
+
+    $('#sum').val(parseFloat(sum).toFixed(2));
+</script>
