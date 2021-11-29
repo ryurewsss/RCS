a78d504bd75d24c1b9bfd9b6b3e2e3f90f9f2b3c
Bryantz <tiwaza13@gmail.com>
2021-11-29 11:40:27 +0700

    Update main.php

--- application/views/main.php
+++ application/views/main.php
@@ -43,7 +43,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
-        <!-- <div class="sidebar-brand-text mx-3">Team <sup>4</sup></div> -->
+        <div class="sidebar-brand-text mx-3">Team <sup>4</sup></div>
       </a>
 
       <!-- Divider -->
