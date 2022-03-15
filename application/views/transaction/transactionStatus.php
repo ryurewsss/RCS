e00afefb641cdbdbfe1ce67fbcc6c89a51532950
ryurewsss <68342195+ryurewsss@users.noreply.github.com>
2022-03-15 15:52:31 +0700

    status

--- application/views/transaction/transactionStatus.php
+++ application/views/transaction/transactionStatus.php
@@ -7,6 +7,12 @@ $tranStatus = array(
     'อยู่ระหว่างการเช่า',
     'เสร็จสิ้นการเช่า',
     'เอกสารถูกแก้ไข',
+
+    'รอการยืนยันเอกสาร',
+    'เอกสารถูกปฏิเสธ',
+    'เอกสารผ่าน <br> รอรับรถสำหรับฝากเช่า',
+    'อยู่ระหว่างการฝากเช่า',
+    'เสร็จสิ้นการฝากเช่า',
 );
 $tranStatusColor = array(
     'text-secondary',
@@ -16,6 +22,12 @@ $tranStatusColor = array(
     'text-warning',
     'text-success',
     'text-primary',
+    
+    'text-primary',
+    'text-danger',
+    'text-primary',
+    'text-warning',
+    'text-success',
 );
 $emailStatus = array(
     'ผิดพลาด',
@@ -25,6 +37,12 @@ $emailStatus = array(
     'ยืนยันการรับรถเช่า',
     'ยืนยันการคืนรถเช่า',
     'ยืนยันการแก้ไขเอกสาร',
+
+    'กรุณายืนยันการฝากเช่า',
+    'เอกสารถูกปฏิเสธกรุณายืนยัน',
+    'เอกสารผ่านกรุณายืนยัน',
+    'ยืนยันการรับรถฝากเช่า',
+    'ยืนยันการคืนรถฝากเช่า',
 );
 $type = array(
     'error',
@@ -34,6 +52,12 @@ $type = array(
     'receive',
     'return',
     'edit',
+
+    'deposit',
+    'deposit_reject',
+    'deposit_confirm',
+    'deposit_receive',
+    'deposit_return',
 );
 $contact = array(
     'email' => 'carrentsystemSE8@gmail.com',
